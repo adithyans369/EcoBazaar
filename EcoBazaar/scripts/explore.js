51c77a95dcf7f7729f089d04c38eef4b4cce666d
aswathi462 <aswathiasw484@gmail.com>
2025-07-30 16:26:37 +0530

    added files

--- EcoBazaar/scripts/explore.js
+++ EcoBazaar/scripts/explore.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <h3>${product.name}</h3>
           <p>${product.description}</p>
           <p>₹${product.price}</p>
-          <button onclick="alert('Buying ${product.name}')">Buy</button>
+         <button onclick="proceedToBuy('${product.id}', '${product.name}', ${product.price}, '${product.image}')">Buy</button>
           <button onclick="alert('Details of ${product.name}')">Details</button>
           <button onclick="addToCart('${product.id}', '${product.name}', ${product.price}, '${product.image}')">Add to Cart</button>
         `;
@@ -46,4 +46,14 @@ function addToCart(id, name, price, image) {
 
   localStorage.setItem('cart', JSON.stringify(cart));
   alert(`${name} added to cart!`);
+}
+function proceedToBuy(id, name, price, image) {
+  localStorage.setItem('selectedProduct', JSON.stringify({
+    id,
+    name,
+    price,
+    image,
+    quantity: 1
+  }));
+  window.location.href = 'buy.html';
 }
